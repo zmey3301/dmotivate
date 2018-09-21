9c6f6a024161b851615cc548643ebf71d68443a0
SoulString <zmey3301@gmail.com>
2018-09-21 22:11:22 +0400

    Style fixes and webkit optimisation
    add: webkit sequence optimisation via will-change
    fix: sequence container paddings

--- style.js
+++ style.js
@@ -1,12 +1,40 @@
+var animationStartEvents =  ["webkitAnimationStart", "animationStart"];
+var animationEndEvents = ["webkitAnimationEnd", "animationend"];
 var sequence = document.getElementsByClassName("smokeContainer")[0];
+var animation
 function hideSequence (event) {
     if (sequence === event.target) {
         document.getElementsByTagName("main")[0].style.display = "block";
         event.target.removeEventListener(event.type, arguments.callee);
     }
 }
-sequence.addEventListener("webkitAnimationStart", hideSequence);
+for (animation of animationStartEvents) sequence.addEventListener(animation, hideSequence);
+
 sequence.addEventListener("animationstart", hideSequence);
+
+// Sequence optimisation via will-change
+var allSmokeText = Array.prototype.slice.call(document.querySelectorAll(".smokeText>h2"));
+var willChange = "transform, filter, opacity, text-shadow";
+allSmokeText[0].style.willChange = willChange;
+var node;
+for (animation of animationEndEvents) {
+    for (node of allSmokeText) {
+        node.addEventListener(animation, function() {
+            node.style.willChange = "auto"
+        }, {once: true});
+    }
+}
+for (animation of animationStartEvents) {
+    for (var key in allSmokeText.slice(0, allSmokeText.length - 2)) {
+        var index = Number(key);
+        if (allSmokeText.hasOwnProperty(index) && typeof allSmokeText[index+1] !== "undefined") {
+            allSmokeText[index].addEventListener(animation, function () {
+                allSmokeText[index+1].style.willChange = willChange;
+            }, {once: true})
+        }
+    }
+}
+
 
 $(document).ready(function() {
     /* Every time the window is scrolled ... */
